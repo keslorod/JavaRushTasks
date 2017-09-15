2cd1e36e29b7b3ce45570c6313da964fdb53576a
DozZzeR <alex.dozer@gmail.com>
2017-09-15 11:42:21 +0300

    Initial commit

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task05/task0530/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task05.task0530;
+
+import java.io.*;
+
+/* 
+Шеф, что-то не пашет
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Character.getNumericValue(reader.read());
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
